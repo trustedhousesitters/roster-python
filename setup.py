3a8b1cbc5983e76d9388eddf2e547d59f32c6ae7
Tim Rijavec <tim@coder.si>
2016-01-08 10:18:29 +0000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='roster',
-    version=":versiontools:roster:",
+    version='1.0',
     description='Roster: A library for simple service discovery using Dynamodb for Python',
     long_description=open('README.md').read(),
     author='Tim Rijavec',
@@ -12,9 +12,6 @@ setup(
     packages=find_packages(exclude=('example', 'examples', )),
     include_package_data=True,
     zip_safe=False,
-    setup_requires=[
-        'versiontools >= 1.6',
-    ],
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
