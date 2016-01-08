8466c05424a9beeb420ffe0fc5b1daf382a5a4b8
Tim Rijavec <tim@coder.si>
2016-01-08 10:03:42 +0000

    python setup package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='roster',
+    version=":versiontools:roster:",
+    description='Roster: A library for simple service discovery using Dynamodb for Python',
+    long_description=open('README.md').read(),
+    author='Tim Rijavec',
+    author_email='tim@trustedhousesitters.com',
+    url='https://github.com/trustedhousesitters/roster-python',
+    license='MIT',
+    packages=find_packages(exclude=('example', 'examples', )),
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[
+        'versiontools >= 1.6',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Service Discovery',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
