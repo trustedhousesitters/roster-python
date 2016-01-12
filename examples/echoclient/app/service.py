50f90a6cb69ef4b3abb719cf036d3b43be270fcb
Tim Rijavec <tim@coder.si>
2016-01-12 15:39:31 +0000

    client reconnect

--- examples/echoclient/app/service.py
+++ examples/echoclient/app/service.py
@@ -18,6 +18,7 @@ def connect(client, service_name='echo'):
     print >>sys.stdout, 'connecting to %s port %s' % (endpoint_data.hostname, endpoint_data.port)
 
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.settimeout(5)
     conn.connect((endpoint_data.hostname, endpoint_data.port))
     return conn, endpoint_data
 
@@ -41,23 +42,32 @@ def main():
 
                 # message = raw_input("Enter your message: ")
                 message = 'My message %d' % message_count
-                message_count+= 1
+                message_count += 1
                 
                 # Send data
                 print >>sys.stdout, 'sending "%s" to "%s:%d"' % (message, endpoint_data.hostname, endpoint_data.port)
-                conn.sendall(message)
+                
+                # conn.setblocking(0)
+                # status = conn.sendall(message)
+                # conn.setblocking(1)
+
+                buffer = message
+                while buffer:
+                    bytes = conn.send(buffer)
+                    buffer = buffer[bytes:]
 
                 # Look for the response
                 amount_received = 0
                 amount_expected = len(message)
                 
+                data = ''
                 while amount_received < amount_expected:
-                    data = conn.recv(1024)
+                    data += conn.recv(1024)
                     amount_received += len(data)
                     if amount_received > 0:
                         print >>sys.stdout, 'received "%s"' % data
 
-                    reconnect_try = 0
+                reconnect_try = 0
             except socket.error, e:
                 # try to reconnect
                 # print >>sys.stderr, 'Socket Error: "%s, %d"' % (str(e), e.errno)
@@ -67,7 +77,6 @@ def main():
                     pass
                 finally:
                     conn = None
-
             except IOError, e:
                 if e.errno == errno.EPIPE:
                     print >>sys.stderr, 'EPIPE IOError : "%s"' % str(e)
